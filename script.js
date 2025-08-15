a3929d8f63927600f04dbda83a98f045acb81a45
IndiaoNaJones <davip.lima99@gmail.com>
2025-08-14 22:18:15 -0300

    :bass: initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  /*if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }*/
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    //se tiver light mode, adicionar a imagem light
+  } else {
+    //se tiver sem o light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+  if (html.classList.contains("light")) {
+    img.setAttribute(
+      "alt",
+      "foto do Mayk Brito sorrindo, de jaqueta de couro preta, óculos escuro e sem barba"
+    )
+  } else {
+    img.setAttribute(
+      "alt",
+      "foto do Mayk Brito sorrindo, com blusa preta, óculos de grau transparente e barba com fundo laranja"
+    )
+  }
+}
